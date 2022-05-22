f3ba94ad62c3d9e7bc6ece799fd29a4c2d6762ed
Vladdos <vlad.ivanov.mh@gmail.com>
2022-05-22 20:39:30 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React from 'react'
 // Components
 import Home from './pages/Home/Home'
 import Teachers from './pages/Teachers/Teachers'
+import Header from './modules/Header/Header'
+import Navbar from './modules/Navbar/Navbar'
 
 const App = () => {
     return (
         <div className='app'>
-            <Home />
+            <Header />
+            <Navbar/>
         </div>
     )
 }
