25806b9c6bd1194d967583b1f325b14bd9178085
Slavka <slavik.nukutuk@gmail.com>
2022-05-26 16:44:21 +0300

    Changed names of styles

--- src/modules/BurgerMenu/BurgerMenu.js
+++ src/modules/BurgerMenu/BurgerMenu.js
@@ -3,20 +3,18 @@ import React from 'react'
 // Styles
 import style from './index.module.scss'
 
-
-
 const BurgerMenu = ({items}) => {
-   return (
-      <div className={style.menu}>
-         <div className={style.menuContent}>
-            <ul>
-               {items.map(item =>
-                  <li>
-                     <a href={item.href}>{item.value}</a>
-                  </li>
-               )}
-            </ul>
-         </div>
-      </div>
-   )
+    return (
+        <div className={style.menu}>
+            <div className={style.menuContent}>
+                <ul>
+                    {items.map(item =>
+                        <li>
+                            <a href={item.href}>{item.value}</a>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </div>
+    )
 }
