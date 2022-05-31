6839be94f819f5b5c18dc960ee14706ba2ab2b35
Vladdos <vlad.ivanov.mh@gmail.com>
2022-06-01 00:36:55 +0300

    Auto stash before merge of "vlad_na_fronte" and "origin/main"

---
+++ src/pages/Home/HomeNews.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+// Styles
+import styles from './index.module.scss'
+
+// Images
+import HomeNewsFirstPicture from '../../assets/images/HomeNewsFirstPicture.png'
+// Components
+import Title from '../../modules/Title/Title'
+
+
+
+const HomeNews = () => {
+   return (
+      <section className={styles.homeNews}>
+         <div className={styles.container}>
+            <div className={styles.hoveNewsTitleWrapper}><Title className={styles.homeNewsTitle} contents = "Новини"/></div>
+            <div className={styles.homeNewsItems}>
+               <div className={styles.homeNewsItem}>
+                  <img src={HomeNewsFirstPicture} alt="HomeNewsFirstPicture"/>
+                  <div className={styles.homeNewsItemContent}>
+                     <h4 className={styles.homeNewsItemTitle}>Чотири десятиліття розробок</h4>
+                     <p className={styles.homeNewsItemText}>В лютому 2022 року знаменна для багатьох науковців університету дата – 40-річчя заснування відділу...</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+   )
+}
+
+export default HomeNews
